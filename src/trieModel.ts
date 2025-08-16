f96b122e1c491210ce4ce530fa5f6b2205197725
JingWen <jingwenhuang1008@gmail.com>
2025-08-16 19:43:42 +0800

    tmp

--- src/trieModel.ts
+++ src/trieModel.ts
@@ -53,7 +53,6 @@ export function SearchTrie(trie: Trie, key: string): Note.BlockValue[][] | null 
 export class Service {
   public static UpdateTrie(saveData: Chrome.ChromeSaveData): Chrome.ChromeSaveData {
     const blockCollection = saveData.notes.flatMap((note) => note.blocks);
-    console.log(blockCollection.length);
     saveData.trie = this.makeTrie(blockCollection);
     return saveData;
   }
